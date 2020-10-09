37c8a5481a4a948b11b390ff4f2281834d76334e
Robbo <robbie.volker@gmail.com>
2020-10-09 23:30:02 +0100

    Exercises part 2

--- RB100/intro_to_ruby/exercises.rb
+++ RB100/intro_to_ruby/exercises.rb
@@ -8,7 +8,7 @@ end
 
 puts "================="
 
-# Exercise 2 : Same as above but prints out values greater than 5 
+# Exercise 2 : Same as above but prints out values greater than 5.
 
 array.each do |e|
   if e > 5 
@@ -18,7 +18,7 @@ end
 
 puts "================="
 
-# Exercise 3 : Use the select method to make a new array of odd numbers
+# Exercise 3 : Use the select method to make a new array of odd numbers.
 
 odd_arr = []
 
@@ -27,7 +27,30 @@ array.select do |num|
     odd_arr << num
   end
 end
+
 puts odd_arr
 puts "================="
 
+# Exercise 4 : Add 0 to the start of the array and 11 to the end of the array. 
 
+array.unshift(0)
+array.push(11)
+
+puts array
+puts "================="
+
+# Exercise 5 : Remove 11, then remove 3 as well. 
+
+array.pop
+array.push(3)
+
+puts array
+puts "================="
+
+# Exercise 6 : Get rid of duplicates in an array. 
+
+array.uniq!
+
+puts array
+
+# Exercise 8 : Create a hash with one key-value pair, using both forms of the Ruby syntax. 
