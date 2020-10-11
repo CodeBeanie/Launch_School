dd06b5fe4f19eb5c16d69275eb45739881a0b91a
Robbo <robbie.volker@gmail.com>
2020-10-11 18:13:51 +0100

    Final exercises from Intro to Ruby book

--- RB100/intro_to_ruby/exercises.rb
+++ RB100/intro_to_ruby/exercises.rb
@@ -71,3 +71,42 @@ h[:e] = 5
 
 h.delete_if {|k, v| v < 3.5}
 
+# Exercise 11 : Write a program that copies information from the array into the empty hash
+
+contact_data = [["joe@email.com", "123 Main st.", "555-123-4567"],
+            ["sally@email.com", "404 Not Found Dr.", "123-234-3454"]]
+
+contacts = {"Joe Smith" => {}, "Sally Johnson" => {}}
+
+contacts["Joe Smith"][:email] = contact_data[0][0]
+contacts["Joe Smith"][:address] = contact_data[0][1]
+contacts["Joe Smith"][:phone] = contact_data[0][2]
+contacts["Sally Johnson"][:email] = contact_data[1][0]
+contacts["Sally Johnson"][:address] = contact_data[1][1]
+contacts["Sally Johnson"][:phone] = contact_data[1][2]
+
+p contacts
+
+# Exercise 12 : Access Joe's email and Sally's phone number.
+
+puts "Joe's email is #{contacts["Joe Smith"][:email]}"
+puts "Sally's number is #{contacts["Sally Johnson"][:phone]}"
+
+# Exercise 13 : Use delete_if and start_with? to delete words that begin with an "s".
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if {|ele| ele.start_with?("s", "w")}
+
+p arr
+
+# Exercise 14 : Split the array on the spaces and create a new array. 
+
+a = ['white snow', 'winter wonderland', 'melting ice',
+     'slippery sidewalk', 'salted roads', 'white trees']
+
+a = a.map { |ele| ele.split(" ") }
+a = a.flatten
+p a
+
+
